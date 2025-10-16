236a005e13bc390ccb1716a0fe2688fc6549ec25
andreaG-student-its24 <andrea.giovene@edu-its.it>
2025-10-16 12:19:15 +0200

    modificato server.js per gestire il login

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Importo la libreria Express
 import express from 'express';
-import mongoose from 'mongoose';
-// Importa e usa le rotte di autenticazione
-// Tutte le rotte in authRoutes inizieranno con /api/auth
-import authRoutes from '../routes/authRoutes';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+// Carico le variabili d'ambiente dal file .env
+dotenv.config();
+
+//Esegui la connessione al DataBase
+connectDB();
 
 // Creo un'stanza dell'applicazione Express
 const app = express();
@@ -11,18 +16,20 @@ const app = express();
 //Middleware per leggere JSON nel corpo della richiesta
 app.use(express.json()); 
 
-// Definiamo la porta su cui il server ascolterà
-// Usiamo la porta 3000 
-const PORT = 3000;
-
-// Rotta di prova
-//(http://localhost:3000/),
-// il server risponderà con un messaggio di benvenuto.
 app.get('/', (req, res) => {
   res.send('Benvenuto su EventHub API!');
 });
 
-// 5. Avviamo il server e lo mettiamo in ascolto sulla porta definita
+// Utilizzo delle rotte di autenticazione
+// Tutte le rotte definite in authRoutes saranno precedute da '/api/auth'
+// Esempio: /api/auth/register, /api/auth/login
+app.use('/api/auth', authRoutes); // <-- NOVITÀ: Collega le rotte all'app
+
+// Definiamo la porta su cui il server ascolterà
+// Usa la porta dal file .env, oppure la 5000 come default
+const PORT = process.env.PORT || 5000; // <-- CORREZIONE 3: Uso di process.env
+
+// Avviamo il server e lo mettiamo in ascolto sulla porta definita
 app.listen(PORT, () => {
-  console.log(`Server in esecuzione sulla porta ${PORT}`);
+  console.log(` Server in esecuzione sulla porta ${PORT}`);
 });
